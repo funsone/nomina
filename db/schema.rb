5f827013ab08ed3b6e38289a7100563c8e3f23ee
dovahv <victorfidelserrano18@gmail.com>
2016-03-13 18:05:31 -0430

    nuevos modulos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313210034) do
+ActiveRecord::Schema.define(version: 20160313222132) do
 
   create_table "departamentos", force: :cascade do |t|
     t.string   "nombre"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20160313210034) do
 
   add_index "departamentos", ["sede_id"], name: "index_departamentos_on_sede_id"
 
+  create_table "departments", force: :cascade do |t|
+    t.string   "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lista", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "sede_id"
@@ -30,6 +36,33 @@ ActiveRecord::Schema.define(version: 20160313210034) do
   end
 
   add_index "lista", ["sede_id"], name: "index_lista_on_sede_id"
+
+  create_table "payrolls", force: :cascade do |t|
+    t.string   "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.integer  "payroll_id"
+    t.boolean  "tipo_cedula"
+    t.string   "nombres"
+    t.string   "apellidos"
+    t.string   "telefono_fijo"
+    t.string   "telefono_movil"
+    t.date     "fecha_de_nacimiento"
+    t.string   "correo"
+    t.string   "direccion"
+    t.boolean  "sexo"
+    t.integer  "estado_civil"
+    t.integer  "grado"
+    t.integer  "status"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "cedula"
+  end
+
+  add_index "people", ["payroll_id"], name: "index_people_on_payroll_id"
 
   create_table "personas", force: :cascade do |t|
     t.integer  "tipo_cedula"
@@ -51,6 +84,17 @@ ActiveRecord::Schema.define(version: 20160313210034) do
   end
 
   add_index "personas", ["lista_id"], name: "index_personas_on_lista_id"
+
+  create_table "positions", force: :cascade do |t|
+    t.string   "titulo"
+    t.decimal  "sueldo"
+    t.boolean  "sueldo_minimo"
+    t.integer  "department_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "positions", ["department_id"], name: "index_positions_on_department_id"
 
   create_table "sedes", force: :cascade do |t|
     t.string   "nombre"
