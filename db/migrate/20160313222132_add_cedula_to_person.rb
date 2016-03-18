9035f345ef35f9efdf173266ff7d49502d8ba50e
dovahv <victorfidelserrano18@gmail.com>
2016-03-18 09:17:17 -0430

    eliminado sqlite

--- db/migrate/20160313222132_add_cedula_to_person.rb
+++
@@ -1,5 +0,0 @@
-class AddCedulaToPerson < ActiveRecord::Migration
-  def change
-    add_column :people, :cedula, :string, :unique => true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,55 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313222132) do
+ActiveRecord::Schema.define(version: 20160318132833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "departments", force: :cascade do |t|
     t.string   "nombre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "headquarter_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  create_table "payrolls", force: :cascade do |t|
+  add_index "departments", ["headquarter_id"], name: "index_departments_on_headquarter_id", using: :btree
+
+  create_table "headquarters", force: :cascade do |t|
     t.string   "nombre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "people", force: :cascade do |t|
-    t.integer  "payroll_id"
-    t.boolean  "tipo_cedula"
-    t.string   "nombres"
-    t.string   "apellidos"
-    t.string   "telefono_fijo"
-    t.string   "telefono_movil"
-    t.date     "fecha_de_nacimiento"
-    t.string   "correo"
-    t.string   "direccion"
-    t.boolean  "sexo"
-    t.integer  "estado_civil"
-    t.integer  "grado"
-    t.integer  "status"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "cedula"
-  end
-
-  add_index "people", ["payroll_id"], name: "index_people_on_payroll_id", using: :btree
-
-  create_table "positions", force: :cascade do |t|
-    t.string   "titulo"
-    t.decimal  "sueldo"
-    t.boolean  "sueldo_minimo"
-    t.integer  "department_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "positions", ["department_id"], name: "index_positions_on_department_id", using: :btree
-
-  add_foreign_key "people", "payrolls"
-  add_foreign_key "positions", "departments"
+  add_foreign_key "departments", "headquarters"
 end
