a3d8b62b8da6bc65fcf39b71e31c394b58cecbe6
victor serrano <victorfidelserrano18@gmail.com>
2016-07-15 16:02:16 -0400

    mysql

--- db/migrate/20160430204909_create_registros.rb
+++ db/migrate/20160430204909_create_registros.rb
@@ -3,13 +3,12 @@ class CreateRegistros < ActiveRecord::Migration
     create_table :registros do |t|
       t.integer :elemento
 
-      t.jsonb :cambios, null: false, default: '{}'
+      t.text :cambios
       t.integer :clase
       t.references :usuario, index: true, foreign_key: true, default: 1
       t.integer :tipo_de_accion
       t.timestamps null: false
     end
-    add_index  :registros, :cambios, using: :gin
 
   end
 end
