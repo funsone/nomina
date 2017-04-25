039123eb4deef86919db77772c7b2a0823900970
funsone <informatica.funsone@gmail.com>
2017-04-25 11:14:29 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
    address:              "smtp.gmail.com",
    port:                 587,
    user_name:            'noresponder.funsone.rrhh@gmail.com',
-   password:             'funsonenomina',
+   password:             'funsonenomina1234',
    authentication:       "plain",
   #enable_starttls_auto: true
   }
